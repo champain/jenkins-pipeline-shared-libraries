48757cf6109f7b0aa3e4c6dc3b742b5d9afb3884
acrewdson <acrewdson@users.noreply.github.com>
2017-10-06 10:12:22 -0400

    ...

--- src/gov/cfpb/PipelineUtils.groovy
+++ src/gov/cfpb/PipelineUtils.groovy
@@ -22,15 +22,12 @@ class PipelineUtils {
         def buildFixed = curBuild == Result.SUCCESS && prevBuild != curBuild && prevBuild != null
         def sendEmail = (curBuild in [Result.FAILURE, Result.UNSTABLE]) || buildFixed
         """
-        def sendEmail = true
-        if (sendEmail && emailList) {
-            return emailext (
-                recipientProviders: [[$class: "RequesterRecipientProvider"]],
-                to: emailList.join(", "),
-                subject: 'test subject', // subject: "\$DEFAULT_SUBJECT",
-                body: 'test body' //body: "\$DEFAULT_BODY"
-            )
-        }
+        emailext (
+            recipientProviders: [[$class: "RequesterRecipientProvider"]],
+            to: emailList.join(", "),
+            subject: 'test subject', // subject: "\$DEFAULT_SUBJECT",
+            body: 'test body' //body: "\$DEFAULT_BODY"
+        )
     }
 
 }

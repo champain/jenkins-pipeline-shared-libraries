a027b48e66f8f62aa7212b6894d8e9eeb2a07ce1
acrewdson <acrewdson@users.noreply.github.com>
2017-10-06 12:30:36 -0400

    ...

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -2,18 +2,15 @@ import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 
 def call(RunWrapper currentBuild, List<String> emailList) {
-
     // We are using Result.toString() instead of doing a less-stringly-typed
-    // approach with Result.fromString() and then the Result comparison
-    // methods because Result.fromString() is not available when pipeline
-    // jobs are sandboxed. We'd like for this shared library to be usable
-    // even in sandboxed mode.
+    // approach with Result.fromString() (combined with the Result comparison
+    // methods) because Result.fromString() is not available when pipeline
+    // jobs are sandboxed. We'd like for this shared library to be usable even
+    // in sandboxed mode. For more info, see:
+    // https://wiki.jenkins.io/display/JENKINS/Script+Security+Plugin/#ScriptSecurityPlugin-User%E2%80%99sguide
 
     def currentResult = currentBuild.currentResult
     def previousResult = currentBuild.getPreviousBuild()?.getResult()
-
-    println('xxxxxxxxxxxxxxx')
-    println(previousResult)
 
     def buildFixed = (
         (currentResult == Result.SUCCESS.toString()) &&
@@ -34,4 +31,3 @@ def call(RunWrapper currentBuild, List<String> emailList) {
         )
     }
 }
-

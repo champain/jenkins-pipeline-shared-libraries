7a5855bbb7e6782f9f3cd079611891b045a7cac2
acrewdson <acrewdson@users.noreply.github.com>
2017-10-06 10:19:02 -0400

    switch to vars

---
+++ vars/pipelineUtils.groovy
@@ -0,0 +1,14 @@
+def sendEmail(currentBuild, List emailList) {
+    """
+    def curBuild = currentBuild.currentResult
+    def prevBuild = currentBuild.getPreviousBuild()?.getResult() ?: null
+    def buildFixed = curBuild == Result.SUCCESS && prevBuild != curBuild && prevBuild != null
+    def sendEmail = (curBuild in [Result.FAILURE, Result.UNSTABLE]) || buildFixed
+    """
+    emailext (
+        recipientProviders: [[$class: "RequesterRecipientProvider"]],
+        to: emailList.join(", "),
+        subject: 'test subject', // subject: "\$DEFAULT_SUBJECT",
+        body: 'test body' //body: "\$DEFAULT_BODY"
+    )
+}

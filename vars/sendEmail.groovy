4be7b609a3a49e496ea7dd50ffa3745eaa3df062
acrewdson <acrewdson@users.noreply.github.com>
2017-10-06 10:47:03 -0400

    ...

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,14 +1,25 @@
-def call(currentBuild, List emailList) {
-    """
-    def curBuild = currentBuild.currentResult
-    def prevBuild = currentBuild.getPreviousBuild()?.getResult() ?: null
-    def buildFixed = curBuild == Result.SUCCESS && prevBuild != curBuild && prevBuild != null
-    def sendEmail = (curBuild in [Result.FAILURE, Result.UNSTABLE]) || buildFixed
-    """
-    emailext (
-        recipientProviders: [[$class: "RequesterRecipientProvider"]],
-        to: emailList.join(", "),
-        subject: 'test subject', // subject: "\$DEFAULT_SUBJECT",
-        body: 'test body' //body: "\$DEFAULT_BODY"
+//import hudson.model.Result
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+
+
+def call(RunWrapper currentBuild, List<String> emailList) {
+    def currentResult = currentBuild.currentResult
+    def previousResult = currentBuild.getPreviousBuild()?.getResult()
+
+    def buildFixed = (
+        (currentResult == Result.SUCCESS) &&
+        (currentResult != previousResult) &&
+        (previousResult != null)
     )
+
+    def badResult = currentResult in [Result.FAILURE, Result.UNSTABLE]
+
+    if (buildFixed || badResult) {
+        emailext (
+            recipientProviders: [[$class: "RequesterRecipientProvider"]],
+            to: emailList.join(", "),
+            subject: 'test subject', // subject: "\$DEFAULT_SUBJECT",
+            body: 'test body' //body: "\$DEFAULT_BODY"
+        )
+    }
 }
